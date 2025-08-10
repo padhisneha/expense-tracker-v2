c598ffbbce319abea7b16e8da37a74d40db0866b
Sneha Padhi <130724380+padhisneha@users.noreply.github.com>
2025-08-10 11:43:52 +0530

    updated firebase.js

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnfxDq3aMVOFRfvTZ28IdQnnWpVM_Qsl0",
-  authDomain: "jpnv-cultural.firebaseapp.com",
-  projectId: "jpnv-cultural",
-  storageBucket: "jpnv-cultural.firebasestorage.app",
-  messagingSenderId: "658315176770",
-  appId: "1:658315176770:web:129db775cb2ec0872ab6db"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
